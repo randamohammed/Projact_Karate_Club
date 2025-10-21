a7e40a8090720c7c31f571e166a9bc15dc5d6293
randamohammed <randamuhammad79@gmail.com>
2025-10-21 20:30:29 +0300

    update conniction

--- DataAccsess/clsConnintionKarate.cs
+++ DataAccsess/clsConnintionKarate.cs
@@ -8,7 +8,7 @@ namespace AccessKarateClubPreojact
 {
     public class clsConnintionKarate
     {
-        public static string ConntionSring = $"Server =.;database = Karate Club;User id =sa;Password =123456";
+        public static string ConntionSring = $"Server =.;database = Karate Club;User id =your;Password =yourpassword";
     }
 }
 
